11dc673c12630ee835877acf5c9bde3f69488844
maitanelizarraga <maitane.lizarraga@opendeusto.es>
2026-03-08 22:16:20 +0100

    1

--- eda.py
+++ eda.py
@@ -3,58 +3,91 @@
 def importarcsv(): 
     import pandas as pd 
     df = pd.read_csv("Digital_Payment_Fraud_Detection_Dataset.csv") 
-    print(df.iloc[0]) 
     return df
 
 def initialinspection(df): 
-    print(df.columns) 
-    print(df.dtypes) 
-    print(df.shape) 
-    print(df.isnull().sum()) 
-    print(df.nunique())
+    print("ANALYSIS:")
+    print("First row:" + "\n" + str(df.iloc[0])) 
+    print(" ")
+    print("Data types:" + "\n" + str(df.dtypes)) 
+    print(" ")
+    print("Shape:" + "\n" + str(df.shape)) 
+    print(" ")
+    print("Missing values:" + "\n" + str(df.isnull().sum())) 
+    print(" ")
+    print("Unique values:" + "\n" + str(df.nunique()))
+    print(" ")
 
 def datacleaning(df): 
-    # Eliminamos solo columnas que existan
-    cols_to_drop = [col for col in ["transaction_id"] if col in df.columns]
-    df = df.drop(columns=cols_to_drop)
-
-    # Rellenamos valores nulos en la columna correcta
-    if "transaction_amount" in df.columns:
-        df["transaction_amount"] = df["transaction_amount"].fillna(df["transaction_amount"].mean())
-
+    #
     return df
 
 def eda(df): 
     import matplotlib.pyplot as plt 
     import seaborn as sns 
 
-    #Fraud distribution
-    sns.countplot(x="fraud_label", data=df)
+    # 1. Fraud distribution
+    ax = sns.countplot(x="fraud_label", data=df)
     plt.title("Fraud distribution")
+
+    for p in ax.patches:
+        ax.annotate(
+            str(p.get_height()),
+            (p.get_x() + p.get_width() / 2., p.get_height()),
+            ha='center', va='center',
+            xytext=(0, 10),
+            textcoords='offset points'
+        )
+
     plt.show()
 
-    #Transaction amount distribution
-    sns.histplot(df["transaction_amount"], bins=40)
-    plt.title("Transaction amount distribution")
+    # 2. Device type distribution
+    ax = sns.countplot(x="device_type", data=df)
+    plt.title("Device type distribution")
+
+    for p in ax.patches:
+        ax.annotate(
+            str(p.get_height()),
+            (p.get_x() + p.get_width() / 2., p.get_height()),
+            ha='center', va='center',
+            xytext=(0, 10),
+            textcoords='offset points'
+        )
+
     plt.show()
 
-    #Count of payment types
+    # 3. Previous failed attempts distribution
+    ax = sns.countplot(x="previous_failed_attempts", data=df)
+    plt.title("Previous failed attempts distribution")
+
+    for p in ax.patches:
+        ax.annotate(
+            str(p.get_height()),
+            (p.get_x() + p.get_width() / 2., p.get_height()),
+            ha='center', va='center',
+            xytext=(0, 10),
+            textcoords='offset points'
+        )
+
+    plt.show()
+
+    # 3. Count of payment types
     sns.countplot(y="payment_mode", data=df)
     plt.title("Payment methods")
     plt.show()
 
-    #Hours and quantities transactions
+    # 4. Hours and quantities transactions
     sns.scatterplot(x="transaction_hour", y="transaction_amount", data=df)
     plt.title("Hour vs quantity transactions")
     plt.show()
 
-    #Heatmap with numerical variables
+    # 5. Heatmap with numerical variables
     numeric_df = df.select_dtypes(include=["int64", "float64"])
     sns.heatmap(numeric_df.corr(), annot=True, cmap="coolwarm")
     plt.title("Correlations")
     plt.show()
     
-    #Frauds by payment method
+    # 6. Frauds by payment method
     sns.countplot(x="payment_mode", data=df[df["fraud_label"] == 1])
     plt.title("Frauds by payment method")
     plt.xlabel("Payment method")
